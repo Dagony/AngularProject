f611369df3d8dd9c11098b2f380796ce7001ac5f
Dagony <dagony@gmail.com>
2017-09-26 21:56:52 +0200

    Click header link now to change the content of the page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  loadedItem = 'recipes';
+
+  onNavigate(item: string) {
+    this.loadedItem = item;
+  }
+
 }
