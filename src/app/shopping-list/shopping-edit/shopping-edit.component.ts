b68209685b0178b7e803ced6804e681a949096b5
Dagony <dagony@gmail.com>
2017-11-27 21:59:59 +0100

    Resetting the Form

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -45,6 +45,11 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
       this.slService.addIngredient(newIngredient);
     }
     this.editMode = false;
+    form.reset();
+  }
+
+  onClear() {
+    this.editMode = false;
     this.slForm.reset();
   }
 
