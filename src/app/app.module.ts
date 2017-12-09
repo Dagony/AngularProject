cdd821ac4a54cd3bb2a60cba035584e84c2184f6
Dagony <dagony@gmail.com>
2017-12-09 22:17:09 +0100

    Signing Users Up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,8 +16,10 @@ import { ShoppingListService } from './shopping-list/shopping-list.service';
 import { AppRoutingModule } from './app-routing.module';
 import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
 import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
+
+import { AuthService } from './auth/auth.service';
 import { RecipeService } from './recipes/recipe.service';
-import {DataStorageService} from "./shared/data-storage.service";
+import { DataStorageService } from './shared/data-storage.service';
 import { SignupComponent } from './auth/signup/signup.component';
 import { SigninComponent } from './auth/signin/signin.component';
 
@@ -44,7 +46,7 @@ import { SigninComponent } from './auth/signin/signin.component';
     HttpModule,
     AppRoutingModule
   ],
-  providers: [ShoppingListService, RecipeService, DataStorageService],
+  providers: [ShoppingListService, RecipeService, DataStorageService, AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
