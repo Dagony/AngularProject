ca0f07ad2f79e9cba56b1234b76b92b824c22052
Dagony <dagony@gmail.com>
2017-12-09 21:11:24 +0100

    GETting Bakc the Recipes

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
+import { Http, Response } from '@angular/http';
 import { RecipeService } from '../recipes/recipe.service';
+import { Recipe } from "../recipes/recipe.model";
 
 @Injectable()
 export class DataStorageService {
@@ -10,6 +11,19 @@ export class DataStorageService {
   }
 
   storeRecipes() {
-    return this.http.put('https://ng-recipe-book-ac18b.firebaseio.com/recipes.json', this.recipeService.getRecipes());
+    return this.http.put(
+      'https://ng-recipe-book-ac18b.firebaseio.com/recipes.json',
+      this.recipeService.getRecipes()
+    );
+  }
+
+  getRecipes() {
+    this.http.get('https://ng-recipe-book-ac18b.firebaseio.com/recipes.json')
+      .subscribe(
+        (response: Response) => {
+          const recipes: Recipe[] = response.json();
+          this.recipeService.setRecipes(recipes);
+        }
+      );
   }
 }
