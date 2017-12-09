60cd43f775ee5f7e28fe8dc0e3dfb0da29977e40
Dagony <dagony@gmail.com>
2017-12-09 22:38:45 +0100

    Signing Users In

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,12 +10,7 @@ export class AppComponent implements OnInit {
 
   ngOnInit() {
     firebase.initializeApp({
-      apiKey: "AIzaSyA61KBuwF0Q54WEpHT3z3_7pRxBd30BM-o",
-      authDomain: "ng-recipe-book-ac18b.firebaseapp.com",
-      databaseURL: "https://ng-recipe-book-ac18b.firebaseio.com",
-      projectId: "ng-recipe-book-ac18b",
-      storageBucket: "ng-recipe-book-ac18b.appspot.com",
-      messagingSenderId: "784704007810"
+
     });
   }
 }
