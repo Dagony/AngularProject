40d6fbabdbc4c6ef2c3646fafe4b2c98f7736687
Dagony <dagony@gmail.com>
2017-12-16 15:28:05 +0100

    Multiple Interceptors

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -11,6 +11,7 @@ import { DataStorageService } from '../shared/data-storage.service';
 import { RecipeService } from '../recipes/recipe.service';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AuthInterceptor } from '../shared/auth.interceptor';
+import { LoggingInterceptor } from '../shared/logging.interceptor';
 
 @NgModule({
   declarations: [
@@ -30,7 +31,8 @@ import { AuthInterceptor } from '../shared/auth.interceptor';
     RecipeService,
     DataStorageService,
     AuthService,
-    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true }
+    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: LoggingInterceptor, multi: true }
   ]
 })
 export class CoreModule {
