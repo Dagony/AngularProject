875bff060d776a2ae666672657ddf632141ece53
Dagony <dagony@gmail.com>
2017-12-16 11:45:23 +0100

    Restructuring Services to use the Child Injector

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,14 +7,7 @@ import { ShoppingListModule } from './shopping-list/shopping-list.module';
 import { SharedModule } from './shared/shared.module';
 
 import { AppComponent } from './app.component';
-import { ShoppingListService } from './shopping-list/shopping-list.service';
 import { AppRoutingModule } from './app-routing.module';
-
-import { AuthService } from './auth/auth.service';
-import { RecipeService } from './recipes/recipe.service';
-import { DataStorageService } from './shared/data-storage.service';
-import { AuthGuard } from './auth/auth-guard.service';
-
 
 @NgModule({
   declarations: [
@@ -29,7 +22,6 @@ import { AuthGuard } from './auth/auth-guard.service';
     AuthModule,
     CoreModule
   ],
-  providers: [ShoppingListService, RecipeService, DataStorageService, AuthService, AuthGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -5,6 +5,11 @@ import { AppRoutingModule } from '../app-routing.module';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
 
+import { AuthService } from '../auth/auth.service';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
+import { DataStorageService } from '../shared/data-storage.service';
+import { RecipeService } from '../recipes/recipe.service';
+
 @NgModule({
   declarations: [
     HeaderComponent,
@@ -17,6 +22,12 @@ import { HomeComponent } from './home/home.component';
   exports: [
     AppRoutingModule,
     HeaderComponent
+  ],
+  providers: [
+    ShoppingListService,
+    RecipeService,
+    DataStorageService,
+    AuthService
   ]
 })
 export class CoreModule {
