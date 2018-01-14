50618d031e3a471a455650da41a8c29659261310
Dagony <dagony@gmail.com>
2018-01-14 11:14:44 +0100

    Adding Reducer Logic & Actions

--- src/app/auth/store/auth.reducers.ts
+++ src/app/auth/store/auth.reducers.ts
@@ -1,3 +1,5 @@
+import * as AuthActions from './auth.actions';
+
 export interface State {
   token: string;
   authenticated: boolean;
@@ -7,9 +9,20 @@ const initialState: State = {
   token: null, authenticated: false
 };
 
-export function authReducer(state = initialState, action) {
-
-
-
-  return state;
+export function authReducer(state = initialState, action: AuthActions.AuthActions) {
+  switch (action.type) {
+    case (AuthActions.SIGNUP):
+    case (AuthActions.SIGNIN):
+      return {
+        ...state, authenticated: true
+      };
+    case (AuthActions.LOGOUT):
+      return {
+        ...state,
+        token: null,
+        authenticated: false
+      };
+    default:
+      return state;
+  }
 }
