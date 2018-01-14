946eeeab4389609e707ecd176da95ac798589ec4
Dagony <dagony@gmail.com>
2018-01-14 11:05:16 +0100

    Setting up the Auth Store Files

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,8 +5,7 @@ import {ShoppingListComponent} from './shopping-list/shopping-list.component';
 import {HomeComponent} from './core/home/home.component';
 
 const appRoutes: Routes = [ {path: '', component: HomeComponent}, {
-  path: 'recipes',
-  loadChildren: './recipes/recipes.module#RecipesModule'
+  path: 'recipes', loadChildren: './recipes/recipes.module#RecipesModule'
 }, {path: 'shopping-list', component: ShoppingListComponent} ];
 
 @NgModule({
