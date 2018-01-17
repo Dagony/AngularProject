677038eaa8f06db1c1f3779fc95c0cac7e65fcf6
Mark <mark.dewit@xillio.nl>
2018-01-17 16:26:00 +0100

    Effects - How they Work

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,14 +1,16 @@
 import {Component, OnInit} from '@angular/core';
 import {NgForm} from '@angular/forms';
 
-import {AuthService} from '../auth.service';
+import * as fromApp from '../../store/app.reducers';
+import * as AuthActions from '../store/auth.actions';
+import {Store} from '@ngrx/store';
 
 @Component({
-  selector: 'app-signup', templateUrl: './signup.component.html', styleUrls: [ './signup.component.css' ]
+  selector: 'app-signup', templateUrl: './signup.component.html', styleUrls: ['./signup.component.css']
 })
 export class SignupComponent implements OnInit {
 
-  constructor(private authService: AuthService) {
+  constructor(private store: Store<fromApp.AppState>) {
   }
 
   ngOnInit() {
@@ -17,7 +19,7 @@ export class SignupComponent implements OnInit {
   onSignup(form: NgForm) {
     const email = form.value.email;
     const password = form.value.password;
-    this.authService.signupUser(email, password);
+    this.store.dispatch(new AuthActions.TrySignup({username: email, password: password}));
   }
 
 }
