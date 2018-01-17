c9229277ee348896aaebdef86ba895d0edfa59db
Mark <mark.dewit@xillio.nl>
2018-01-17 16:17:19 +0100

    Auth Effects and Actions

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+///<reference path="../../node_modules/@ngrx/store/src/store_module.d.ts"/>
 import {BrowserModule} from '@angular/platform-browser';
 import {HttpClientModule} from '@angular/common/http';
 import {NgModule} from '@angular/core';
@@ -7,14 +8,17 @@ import {AppComponent} from './app.component';
 import {AppRoutingModule} from './app-routing.module';
 import {SharedModule} from './shared/shared.module';
 import {ShoppingListModule} from './shopping-list/shopping-list.module';
-import {AuthModule} from './auth/auth.module'
+import {AuthModule} from './auth/auth.module';
 import {CoreModule} from './core/core.module';
-import { reducers } from './store/app.reducers';
+import {reducers} from './store/app.reducers';
+import {EffectsModule} from '@ngrx/effects';
+import {AuthEffects} from './auth/store/auth.effects';
 
 @NgModule({
-  declarations: [ AppComponent ],
-  imports: [ BrowserModule, HttpClientModule, AppRoutingModule, SharedModule, ShoppingListModule, AuthModule, CoreModule, StoreModule.forRoot(reducers) ],
-  bootstrap: [ AppComponent ]
+  declarations: [AppComponent],
+  imports: [BrowserModule, HttpClientModule, AppRoutingModule, SharedModule, ShoppingListModule, AuthModule, CoreModule,
+    StoreModule.forRoot(reducers), EffectsModule.forRoot([AuthEffects])],
+  bootstrap: [AppComponent]
 })
 export class AppModule {
 }
